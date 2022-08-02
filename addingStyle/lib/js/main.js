5ffcb2589635fce2c504729f3796400bf61761bf
SammyHaridy <sammyh11@nycstudents.net>
2022-08-02 09:22:05 -0400

    first commit

---
+++ addingStyle/lib/js/main.js
@@ -0,0 +1,6 @@
+document.body.style.backgroundColor = "#d3c26f";
+
+document.getElementsByClassName("name")[0].style.textShadow = "-6px 5px 2px yellow";
+document.getElementsByClassName("name")[1].style.textShadow = "-6px 5px 2px yellow";
+
+
---
+++ theBrush/lib/js/main.js
@@ -0,0 +1,5 @@
+let src = document.querySelector('src')
+let rengar= document.querySelector('#rengar')
+rengar.addEventListener('click', () =>{
+img.src = 'assets/rengar.png'
+})
